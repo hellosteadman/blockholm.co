173397b3e20d3ba208d45b0ddf4f52f225c73ee5
Mark Steadman <mark@origin.fm>
2024-10-31 12:29:39 +0000

    Adds an uptime JSON function
    This is really unnecessary but it means that when I’m making changes to the site, the 503 page can automatically check if the site’s back up and reload the page.

--- sidekick/urls.py
+++ sidekick/urls.py
@@ -1,8 +1,25 @@
 from django.conf import settings
 from django.contrib import admin
+from django.http.response import JsonResponse
 from django.urls import path, re_path, include
+from django.utils import timezone
+from django.views.generic import TemplateView, View
 from sidekick.seo.views import RobotsTxtView
 from . import sitemaps
+
+
+LOADED = timezone.now()
+
+
+class ReadyView(View):
+    def get(self, request):
+        return JsonResponse(
+            {
+                'uptime': int(
+                    (timezone.now() - LOADED).total_seconds()
+                )
+            }
+        )
 
 
 urlpatterns = [
@@ -10,7 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', RobotsTxtView.as_view(), name='robots_txt'),
     path('', include('sidekick.newsletter.urls')),
-    path('', include(sitemaps))
+    path('', include(sitemaps)),
+    path('~/ready/', ReadyView.as_view())
 ]
 
 if settings.DEBUG:
